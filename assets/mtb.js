cff7ef393cb9e458c7c6acfa4ef2c66ea300cbaf
SummerMedusa <50718811+SummerDarcel@users.noreply.github.com>
2019-08-01 19:00:16 -0700

    css

--- assets/mtb.js
+++ assets/mtb.js
@@ -1,9 +1,24 @@
+function searchlastfm() {
+  //    let feeling = $(this).attr("data-name");
+  let queryURL = "https://ws.audioscrobbler.com/2.0/?method=track.search&track=Believe&limit=4&api_key=d6b014786cf900b1834301c65617492d&format=json";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+
+  }).then(function (response) {
+    let results = response.data;
+    // let infoForIframe = results.url;
+    console.log(response);
+    // console.log(results.url);
+  });
+} searchlastfm();
 
 
 $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
-      
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
 
